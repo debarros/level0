233354e0306e9a7f8cc2e5cbd4bf57108dda5970
debarros <pj.debarros@gmail.com>
2019-10-25 14:42:07 -0400

    initial commit

---
+++ R/StudentLite.R
@@ -0,0 +1,74 @@
+# StudentLite.R
+
+studlite = read.csv(file.choose(), stringsAsFactors = F)
+  templates = openxlsx::loadWorkbook(file.choose())
+
+  str(studlite)
+StudentLite = function(studlite, templates){
+  colnames(studlite) = dBtools::GetNiceColumnNames("STUDENT LITE", templates)
+  # Check for missing values
+  # # district code
+  # # location code
+  # # school year
+  # # student id
+  # # last name
+  # # first name
+  # # grade level
+  # # dob
+  # # gender
+  # # race1
+  # # status code
+  # # dor code
+  # # hispanic
+
+  # Check for values in unused columns
+  # #
+
+  # Check for duplicates
+  # # District code
+  # # Student id
+
+  # Check for unacceptable values
+  # # student id length <= 9, numeric
+  # # last name length <= 25
+  # # first name length <= 15
+  # # grade level in list 1:14 two digit, GD, KF, KH, PKF, PKH, PS
+  # # homeroom length <= 6
+  # # dob valid date (make suggestions), earlier than current date, within last 23 years
+  # # gender must be M or F
+  # # Race1 in table
+  # # Race2:5 black or in table
+  # # Home language in table of values
+  # # post grad activity blank or in lookup
+  # # status code in lookup table
+  # # diploma type code blank or in table
+  # # date of entry in grade 9 cannot be in the future
+  # # Address length <= 30
+  # # Address2 length <= 30
+  # # City length <= 25
+  # # State length <= 2
+  # # ZIP length <=10
+  # # Home phone length <= 14
+  # # Guardian name length <= 40
+  # # Guardian2 name length <= 40
+  # # Place of birth <= 50
+  # # dor code in table
+  # # hispanic yes or no
+  # # Homeless y or n, blanks become n
+  # # Migrant  y or n, blanks become n
+  # # Immigrant yes or no or blank
+  # # Neglected y or n, blanks become n
+  # # Career path code in table
+  # #
+
+
+  # Comparing columns
+  # # No duplicate races for one student
+  # # last status date in current school year (usuing school year column)
+  # # diploma type and career path either both exist, or neither exists
+  # # Date of entry in grade 9 must be a valid date if grade level in 9:12, cannot exceed the school year, cannot preceed dob
+  # # guidance counselor district code must be blank or equal district code column
+
+
+
+}
